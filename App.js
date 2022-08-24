a75fb6dbb064c04a3ee0f0df0e1d26f094abfba6
Saurav Bansal <sauravbansal448@gmail.com>
2022-08-24 09:30:49 +0530

    FirstCommit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class App extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          height: 100,
+          backgroundColor:"blue"
+                }}>
+      </View>
+    );
+  }
+}
+
+export default App;
