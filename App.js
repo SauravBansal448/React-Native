b48bae5f06d1870563934721e5e1a07d326a26fe
Saurav Bansal <sauravbansal448@gmail.com>
2022-08-24 09:56:48 +0530

    add Text Component

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
-class App extends Component {
+class TextInANest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleText: "Bird's Nest",
+      bodyText: "This is not really a bird nest."
+    };
+  }
+
+  onPressTitle = () => {
+    this.setState({ titleText: "Bird's Nest [pressed]" });
+  };
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          height: 100,
-          backgroundColor:"blue"
-                }}>
-      </View>
+      <Text style={styles.baseText}>
+        <Text
+          style={styles.titleText}
+          onPress={this.onPressTitle}
+        >
+          {this.state.titleText}
+          {"\n"}
+          {"\n"}
+        </Text>
+        <Text numberOfLines={5}>{this.state.bodyText}</Text>
+      </Text>
     );
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: "Cochin"
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+
+export default TextInANest;
